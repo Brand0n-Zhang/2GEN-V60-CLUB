27f6610cd6ec50174196790c9a13f18b5a5c6fa4
zbd <zhangbaodong@duiba.com.cn>
2022-02-16 17:55:16 +0800

    fix

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -8,4 +8,18 @@ Page({
       animation: true,
     });
   },
+  onShareAppMessage() {
+    // const promise = new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve({
+    //       title: '自定义转发标题',
+    //     });
+    //   }, 2000);
+    // });
+    // return {
+    //   title: '2GEN V60 CLUB',
+    //   path: '/pages/index/index',
+    //   //   promise,
+    // };
+  },
 });
